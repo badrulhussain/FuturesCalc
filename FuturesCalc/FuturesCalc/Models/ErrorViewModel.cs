93ecf1eff8ce0603048cd20f02dc2c8c51035ca8
BadrulHussain <Badrul.Hussain1@gmail.com>
2020-04-07 15:27:10 +0100

    Created the project

---
+++ FuturesCalc/FuturesCalc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FuturesCalc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
